7aaf6ee997e5e7b744aedb256bb9f414108dc8b3
Muhammad Ayyub Abdurrahman <49425375+Ayyub29@users.noreply.github.com>
2020-05-31 03:29:42 +0700

    merapikan

--- src/a_config.php
+++ src/a_config.php
@@ -1,10 +1,10 @@
 <?php
 	switch ($_SERVER["SCRIPT_NAME"]) {
-		case "/StrAlgo4-18076/src/about.php":
+		case "/src/about.php":
 			$CURRENT_PAGE = "About"; 
 			$PAGE_TITLE = "About Us";
 			break;
-		case "/StrAlgo4-18076/src/contact.php":
+		case "/src/contact.php":
 			$CURRENT_PAGE = "Contact"; 
 			$PAGE_TITLE = "Contact Us";
 			break;
